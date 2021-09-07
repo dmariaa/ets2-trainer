eb48e0e89697f797ff2152f0ecf9f5a2c4991b10
David María Arribas <dmariaa70@gmail.com>
2021-09-07 17:49:58 +0200

    New: actual project implementation

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import torch
 from PIL import Image
 from torchvision import transforms, datasets
 from model import Model
-from utils import colorize
+from utils import colorize, depth_norm
 
 base_dir = os.path.dirname(__file__)
 
@@ -33,16 +33,34 @@ def parse_args():
                         help='disable CUDA',
                         action='store_true')
 
+    parser.add_argument('--cuda-device', dest='device_number',
+                        type=int,
+                        help='cuda device/GPU number to use, (default=0)',
+                        default=0)
+
+    parser.add_argument('--dataset',
+                        help='Dataset used to perform test',
+                        choices=['kitti', 'ets2'],
+                        default='ets2')
+
     return parser.parse_args()
 
 
-class Tester():
+class Tester:
     def __init__(self, options):
         self.opt = options
         self.model = Model(pretrained_encoder=False)
 
         self.device = torch.device(
             "cpu" if self.opt.no_cuda or not torch.cuda.is_available() else f"cuda:{self.opt.device_number}")
+
+    def load_image(self, path):
+        input_image = Image.open(path).convert('RGB')
+
+        if self.opt.dataset == 'kitti':
+            input_image.convert('RGB').resize((1248, 384))
+
+        return input_image
 
     def run(self):
         self.load_model()
@@ -51,14 +69,15 @@ class Tester():
         with torch.no_grad():
             for i, image_file in enumerate(images):
                 print(f"predicting depth for {image_file}")
-                input_image = Image.open(image_file).convert('RGB')
-                original_size = input_image.size
-                input_image = input_image.resize((640,480), Image.ANTIALIAS)
-                input_image = transforms.ToTensor()(input_image).unsqueeze(0)
-                input_image.to(self.device)
-                output = self.model(input_image)
 
-                self.save_output(output, original_size, image_file)
+                input_image = self.load_image(image_file)
+                original_size = input_image.size
+                input_image = transforms.ToTensor()(input_image).unsqueeze(0).to(self.device)
+
+                output = self.model(input_image)
+                output = np.clip(depth_norm(output, max_depth=80), 1e-3, 80) / 80
+
+                self.save_output(1 - output, original_size, image_file)
 
     def save_output(self, output, original_size, original_name):
         orig_width, orig_height = original_size
@@ -68,20 +87,16 @@ class Tester():
 
         # save depth
         depth = torch.nn.functional.interpolate(output, (orig_height, orig_width), mode="bilinear", align_corners=False)
-
         print(f"Depth range ({depth.min()},{depth.max()})")
-
         print(f"Depth shape {depth.shape}")
         np.save(os.path.join(self.opt.images_path, f"{depth_name}.npy"), depth[0].data)
 
-
         # save depth colormap
-        colorized_depth = colorize(depth[0])
-
+        colorized_depth = colorize(depth[0], clip=None, normalize=False)
         colorized_depth = colorized_depth.transpose(1, 2, 0)
         print(f"Colorized depth {colorized_depth.shape}")
         im = Image.fromarray(colorized_depth)
-        im.save(os.path.join(self.opt.images_path, f"{depth_name}.jpg"))
+        im.save(os.path.join(self.opt.images_path, f"{depth_name}.depth.jpg"))
 
     def load_model(self):
         models_folder = os.path.join(base_dir, "models", self.opt.model_name)
@@ -93,9 +108,12 @@ class Tester():
         model_path = os.path.join(models_folder, 'model.pth')
 
         model_dict = torch.load(model_path, map_location=self.device)
-        self.model.load_state_dict(model_dict['model_state_dict'])
+        self.model.load_state_dict(model_dict["model_state_dict"])
         self.model.to(self.device)
         self.model.eval()
+
+        checkpoint_date = model_dict['checkpoint']
+        print(f"model {self.opt.model_name} generated on {checkpoint_date.strftime('%d/%m/%Y %H:%M:%S')}")
 
 
 if __name__ == '__main__':
